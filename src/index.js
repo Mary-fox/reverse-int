b2e47a7ac1e81161d59e3adb6bcf9ee693848c50
Mary <89971822+Mary-fox@users.noreply.github.com>
2023-01-11 17:32:17 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+const { neq } = require("semver");
+
 module.exports = function reverse (n) {
-  
+let str = n.toString();
+if (n < 0) return reverse(-n);
+let reverseStr = str.split("").reverse().join("");
+
+ return reverseStr;
+
 }
